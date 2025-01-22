4c2d00bbe4c6b033a6e7750ecdd7d0cdc908c96c
ray-cod <raimilassissi8@gmail.com>
2025-01-22 15:43:40 +0200

    Created scheleton

---
+++ src/main/java/com/raimi_dikamona/visorpost/VisorpostApplication.java
@@ -0,0 +1,13 @@
+package com.raimi_dikamona.visorpost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisorpostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisorpostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raimi_dikamona/visorpost/VisorpostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raimi_dikamona.visorpost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisorpostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
