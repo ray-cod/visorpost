ff823e2e39763b87ddbdb1a4c35958ad73a3672d
ray-cod <raimilassissi8@gmail.com>
2025-01-26 15:58:31 +0200

    Created security congiguration class

---
+++ src/main/java/com/raimi_dikamona/visorpost/config/SecurityConfiguration.java
@@ -0,0 +1,37 @@
+package com.raimi_dikamona.visorpost.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.*;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+
+    private final JwtAuthenticationFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf(AbstractHttpConfigurer::disable) // Disable CSRF
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("").permitAll() // Allow certain endpoints
+                        .anyRequest().authenticated() // All other endpoints require authentication
+                )
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // Stateless session management
+                )
+                .authenticationProvider(authenticationProvider) // Custom authentication provider
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class); // Add JWT filter before username-password authentication filter
+
+        return httpSecurity.build();
+    }
+}
