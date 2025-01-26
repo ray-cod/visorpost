8b6ab5a3c1b2694b5e4c25c28a98e1347430f943
ray-cod <raimilassissi8@gmail.com>
2025-01-26 19:06:33 +0200

    Created Authetication service and completed AuthController

--- src/main/java/com/raimi_dikamona/visorpost/Controllers/auth/AuthController.java
+++ src/main/java/com/raimi_dikamona/visorpost/Controllers/auth/AuthController.java
@@ -1,5 +1,6 @@
 package com.raimi_dikamona.visorpost.Controllers.auth;
 
+import com.raimi_dikamona.visorpost.services.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -9,14 +10,16 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/auth")
 public class AuthController {
 
+    private final AuthService service;
+
     @PostMapping("/register")
     public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request){
-        return null;
+        return ResponseEntity.ok(service.register(request));
     }
 
     @PostMapping("/authenticate")
     public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request){
-        return null;
+        return ResponseEntity.ok(service.authenticate(request));
     }
 
 
---
+++ src/main/java/com/raimi_dikamona/visorpost/services/AuthService.java
@@ -0,0 +1,60 @@
+package com.raimi_dikamona.visorpost.services;
+
+import com.raimi_dikamona.visorpost.Controllers.auth.*;
+import com.raimi_dikamona.visorpost.config.JwtService;
+import com.raimi_dikamona.visorpost.models.*;
+import com.raimi_dikamona.visorpost.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.*;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.NoSuchElementException;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationResponse register(RegisterRequest request){
+        User user = User.builder()
+                .firstName(request.getFirstName())
+                .lastName(request.getLastName())
+                .bio("")
+                .profilePicture("")
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .createdAt(LocalDateTime.now())
+                .googleId("")
+                .build();
+        repository.save(user);
+        String jwtToken = jwtService.generateToken(user);
+
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request){
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+        User user = repository.findByEmail(request.getEmail())
+                .orElseThrow(() -> new NoSuchElementException("The user with the email \""
+                        + request.getEmail() + "\" doesn't exist"));
+        String jwtToken = jwtService.generateToken(user);
+
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+}
